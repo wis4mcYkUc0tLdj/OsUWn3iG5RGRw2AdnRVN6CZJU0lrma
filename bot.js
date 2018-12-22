024ec5df75a3d667ec65766c64ac49afa3aadbae
wis4mcYkUc0tLdj <46072660+wis4mcYkUc0tLdj@users.noreply.github.com>
2018-12-22 17:06:56 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**CAN YOU JOIN PLEASE? :heart_eyes: :hearts: https://discord.gg/ExPYXcN :relaxed:**`)
+
+}).catch(console.error)
+})
+
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**CAN YOU JOIN PLEASE? :heart_eyes: :hearts: https://discord.gg/ExPYXcN :relaxed:**`)
+
+}).catch(console.error)
+})
+client.login('"NTIzODE1MjIxOTI4Nzg3OTY5.Dv_XvA.-P-2Ir-9jfdMQdbSNHUTRPSe7aM"');
